9724306e056fd27b2f45af91c60b734dfff776cc
admin-nsi <david.wrobel@ac-lille.fr>
2021-11-19 08:26:08 +0100

    Ajout du prof

---
+++ src/grid.py
@@ -0,0 +1,33 @@
+class Grid:
+    
+    def __init__(self, puzzle = 81*'.'):
+        """
+            Constructeur par défaut
+            Arguments :
+                - puzzle : chaine de 81 caractères
+            Tests :
+            >>> Grid().puzzle == 81*'.', Grid(81*'2').puzzle == 81*'2'
+            (True, True)
+            >>> Grid().full, Grid(81*'2').full
+            (False, True)
+        """
+        self.puzzle = puzzle
+        self.full = "." not in self.puzzle
+        
+    def loadFromFile(num):
+        """
+            Charger un puzzle dans le fichier grids.sud
+            Argument :
+                - num : numéro du puzzle à charger
+            Tests :
+            >>> Grid.loadFromFile(0).puzzle[:10]
+            '4.....8.5.'
+        """
+        file = open("data/grids.sud")
+        lines = file.readlines()
+        return Grid(lines[num][:-1]) 
+    
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+        
